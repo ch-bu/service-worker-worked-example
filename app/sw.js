cc222fbdf1ca24dff2efe5119f8fb5ddfa6ac077
Christian Burkhart <burkhart.christian@gmail.com>
2017-12-03 21:01:26 +0100

    feat: cache new request not yet in cache

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,11 @@
-var CACHE_NAME = "v2";
+// Examples of service worker implementations:
+// https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker
+
+var CACHE_NAME = "v3";
 var urlsToCache = [
-  '/wonderful.jpg'
+  '/wonderful.jpg',
+  '/cat.jpg',
+  '/my404.html'
 ];
 
 self.addEventListener('install', function(event) {
@@ -13,14 +18,45 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  // Respond with is a method from the FetchEvent.
+  // It prevents the browser's default fetch
+  // handling. It allows me to fetch my custom stuff.
   event.respondWith(
+    // Checks if a specific request is in any of the caches.
     caches.match(event.request)
+      // The fullfill state deals with all request, even those
+      // which are not cached. Therefore, we do not need
+      // a reject (catch) for the promise.
       .then(function(response) {
         if (response) {
           return response;
         }
 
-        return fetch(event.request);
+        // Requests which are not in the request are fetched
+        return fetch(event.request)
+          .then(function(response) {
+            // If there response yields a 404 show user
+            // custom 404 page
+            if (response.status === 404) {
+              return caches.match('/my404.html');
+            }
+
+            // If we haven't fetched this request yet,
+            // store it in the cache
+            return caches.open('v3')
+              .then(function(cache) {
+                return fetch(event.request)
+                  .then(function(response) {
+                    return cache.put(event.request, response.clone())
+                      .then(function() {
+                        return response;
+                      });
+                  });
+              });
+          })
+          .catch(function() {
+            return new Response('That totally failed');
+          });
       })
   )
 });
